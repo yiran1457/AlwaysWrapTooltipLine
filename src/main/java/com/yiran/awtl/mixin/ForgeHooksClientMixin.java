2bbe2ca1c17cc8efe518b840f3a1f22c7f7bcadf
yiran1457 <3270897489@qq.com>
2026-07-06 18:52:42 +0800

    启用mixin

--- src/main/java/com/yiran/awtl/mixin/ForgeHooksClientMixin.java
+++ src/main/java/com/yiran/awtl/mixin/ForgeHooksClientMixin.java
@@ -5,7 +5,7 @@ import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.ModifyVariable;
 
-@Mixin(ForgeHooksClient.class)
+@Mixin(value = ForgeHooksClient.class,remap = false)
 public class ForgeHooksClientMixin {
     @ModifyVariable(
             method = "gatherTooltipComponentsFromElements",
